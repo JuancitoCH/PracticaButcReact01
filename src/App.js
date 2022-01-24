d5d15dd6d2f2e3cfddec560deb7f9c70319c2a3e
JuancitoCH <cjuan.chona@hotmail.com>
2022-01-24 01:06:58 -0300

    Agregado una eliminacion del ultimo componente

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Card from './components/Card'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = ()=>{
+  
+  const [tarjetas,setTarjetas] = useState([])
+  
+  const agregarTarjeta = () =>{
+    tarjetas.push(<Card key={tarjetas.length} titulo={`La tarjeta numero ${tarjetas.length}`} numero={tarjetas.length} mequieromori={EliminarUna}/>)
+    setTarjetas([...tarjetas])
+  }
+  const eliminarTarjetaUltima=()=>{
+    const array = tarjetas.filter(function(el){
+      return el.key !== ((tarjetas.length - 1 +''))
+    })
+    setTarjetas(array)
+  }
+
+  const EliminarUna=(id)=>{
+    console.log(tarjetas)
+    const array = tarjetas.filter(function(el){
+      console.log('key ' +el.key)
+      console.log('id '+id)
+
+      return el.key !== id
+    })
+    console.log(array)
+    setTarjetas(array)
+  }
+ 
+  return(
+    <div>
+      <h2>Prueba de css</h2>
+      <button onClick={agregarTarjeta}>Agregar tarjeta</button>
+      <button onClick={eliminarTarjetaUltima}>DEl</button>
+      <div className='cuadricula'>
+        {tarjetas}
+      </div>
+      
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
+
---
+++ src/components/Card.jsx
@@ -0,0 +1,30 @@
+import React,{useState} from 'react';
+import '../css/Card.css'
+
+export default function Card(props) {
+  
+    const [numero,setNumero] = useState(0) // 5: valor inicial
+    
+    const aumentar = ()=>{
+        setNumero(numero+1)
+    }
+    const del=()=>{
+      const id = toString(props.numero)
+      props.mequieromori(id)
+      
+    }
+
+  return <div className='card'>
+        {/* Esta entre llaves porque soy una expresion de JS */}
+      <h2>{props.titulo}</h2>
+      <p>Mi contenido {props.numero}</p>
+      <p id='contenido'>Numero: {numero}</p>
+      <p>Numero: {numero}</p>
+      <p>Numero: {numero}</p>
+      <p>Numero: {numero}</p>
+      <button onClick={aumentar}>Aumentar</button>
+      <button onClick={()=>{setNumero(0)}}>Reset</button>
+      <button onClick={del}>Eliminar</button>
+      {/* Mi comentario */}
+  </div>;
+}
